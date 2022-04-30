8c31a72b881cb9698600dbb5f660e5740844b98d
ADSM <yesform2018@gmail.com>
2022-05-01 02:57:25 +0600

    post method is completed

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 require('dotenv').config()
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ii6vt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('db conected');
-    client.close();
-});
 
+const run = async () => {
+
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('inventory').collection('cars');
+
+        app.post('/inventory', async (req, res) => {
+            const newInventory = req.body;
+            const result = await inventoryCollection.insertOne(newInventory);
+            res.send(result);
+        })
+        console.log('bla');
+    }
+    finally {
+        // client.close();
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
