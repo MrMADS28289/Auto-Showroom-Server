b878650e70053c36cce5228d1a63c6460a1c6e02
ADSM <yesform2018@gmail.com>
2022-05-06 04:10:40 +0600

    jwt completed

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const tokenInfo = req.headers.authorization;
+
+    if (!tokenInfo) {
+        return res.status(401).send({ message: 'Unouthorize access' })
+    }
+    const token = tokenInfo.split(' ')[1];
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        else {
+            req.decoded = decoded;
+            next();
+        }
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ii6vt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,8 +37,16 @@ const run = async () => {
         await client.connect();
         const inventoryCollection = client.db('inventory').collection('cars');
 
+        // use jwt
+        app.post('/login', (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.SECRET_KEY)
+            // console.log(email, token);
+            res.send({ token });
+        })
+
         // add inventory
-        app.post('/inventory', async (req, res) => {
+        app.post('/inventory', verifyJWT, async (req, res) => {
             const newInventory = req.body;
             const result = await inventoryCollection.insertOne(newInventory);
             res.send(result);
@@ -40,12 +67,19 @@ const run = async () => {
             res.send(car);
         })
 
-        app.get('/myinventory', async (req, res) => {
-            const email = req.query.email;
-            const query = { userEmail: email };
-            const cursor = inventoryCollection.find(query);
-            const cars = await cursor.toArray();
-            res.send(cars);
+        app.get('/myinventory', verifyJWT, async (req, res) => {
+            const decodedEmail = req?.decoded?.email;
+            const email = req?.query?.email;
+            console.log(decodedEmail, email);
+            if (email === decodedEmail) {
+                const query = { userEmail: email };
+                const cursor = inventoryCollection.find(query);
+                const cars = await cursor.toArray();
+                res.send(cars);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
         // update car
@@ -92,3 +126,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Auto showroom server running in port', port);
 })
+
+// https://git.heroku.com/auto-shoroom.git
